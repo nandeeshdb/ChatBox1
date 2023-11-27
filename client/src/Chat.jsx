b528df9d0e6bc5f8ab28c1994db85ef401fd2912
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-11-28 01:01:20 +0530

    selecting conversation

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,11 +1,22 @@
 
 import React, { useEffect, useState } from 'react'
 import Avathar from './Avathar'
+import Logo from './Logo'
+import { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 function Chat() {
 
     const[ws,setWs]= useState(null)
     const[onlinePeople,setOnlinePeople] =useState({})
+    const[selectedUserId,setSelectedUserId] = useState(null)
+
+    const{username,userId:id} = useContext(UserContext)
+
+
+    const onlinePeopleExcOurs = {...onlinePeople}
+
+    delete onlinePeopleExcOurs[id]
 
 
     useEffect(()=>{
@@ -35,28 +46,33 @@ function Chat() {
     }
   return (
     <div className='flex h-screen'>
-        <div className='bg-blue-50 100 w-1/3 pt-4 pl-4 sm:w-1/4'>
-            <div className='font-bold text-blue-600 flex gap-3 mb-4'>
-                
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-  <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
-  <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
-</svg>
-ChatBox
-
-            </div>
-        
-        {Object.keys(onlinePeople).map(userId=>(
-            <div className='border-b border-gray-200 py-2 flex  items-center gap-8' >
+        <div className='bg-white  w-1/3 pt-4 pl-4 sm:w-1/4'>
+            <Logo />
+        {Object.keys(onlinePeopleExcOurs).map(userId=>(
+            <div onClick={()=>setSelectedUserId(userId)}  >
+                <div key={userId} className={"border-b border-gray-200 py-5 flex  items-center gap-8 cursor-pointer" + (userId===selectedUserId ? 'bg-blue-500' : '')} >
+                    {
+                        userId===selectedUserId && (
+                            <div className='w-1 h-12 bg-blue-600'></div>
+                        )
+                    }
                 <Avathar username={onlinePeople[userId] } userId={userId} />
                 <span className='text-xl sm:text-2xl '>{onlinePeople[userId]}</span>
+                </div>
             </div>
         ))}
         </div>
 
         <div className='flex flex-col bg-blue-200 w-3/4 p-4 sm:w-3/4 '>
            
-           <div className='flex-grow'>message of selected contacts</div> 
+           <div className='flex-grow'>
+           {
+            !selectedUserId && (
+                <div className='flex h-full items-center justify-center text-gray-400 text-3xl'>&larr; Please select a person to chat</div>
+            )
+           }
+            
+            </div> 
 
             <div className='flex gap-2 '>
                 <input  
